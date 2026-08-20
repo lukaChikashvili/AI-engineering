1b77b566e9eb5a83adb0131daba6aa5411a938d1
luka chikashvili <lukachikashvili2020@gmail.com>
2026-08-20 16:00:41 +0400

    add fast api first message

--- PYTHON/nummpy.py
+++ PYTHON/nummpy.py
@@ -10,6 +10,27 @@ print(np1[3:])   # [4 5 6 7 8 9] dont specify the end
 print(np1[1:5:2])   # every other element
 print(np1[::2])  # steps to entire array
 
+# ----------------------------------------
+
+# numpy universal functions
+
+np1 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(np1)
+
+# square root of each element
+#print(np.sqrt(np1))
+
+# absolute value
+#print(np.absolute(np1))
+
+# copy vs view
+
+np2 = np1.view()
+
+print(f'original array 1 {np1}')
+print(f'original array 2 {np2}')
+
+
 
 
 
