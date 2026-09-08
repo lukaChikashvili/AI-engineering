b8d100f75b29baf07514ff7c5f61707672e2d458
luka chikashvili <lukachikashvili2020@gmail.com>
2026-09-08 12:06:38 +0400

    add move zeros problem

--- DSA/INTRO/main.js
+++ DSA/INTRO/main.js
@@ -59,3 +59,29 @@ var reverseString = function(s) {
 
 
 console.log(reverseString(["h","e","l","l","o"]));
+
+
+// Given an integer array nums, move all 0's to the end of it while maintaining 
+// the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+//Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+var moveZeroes = function(nums) {
+     let x = 0;
+
+     for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[x] = nums[i];
+            x++;
+
+
+        }
+     }
+
+     for(let i = x; i < nums.length; i++) {
+        nums[i] = 0;
+        
+     }
+
+};
