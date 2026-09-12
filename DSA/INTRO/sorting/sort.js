d8070e5cb368f842c15b9a46b4167db0e06ce482
luka chikashvili <lukachikashvili2020@gmail.com>
2026-09-12 16:47:35 +0400

    add bubble sort

---
+++ DSA/INTRO/sorting/sort.js
@@ -0,0 +1,20 @@
+// bubble sort
+
+const bubbleSort = (arr) => {
+   let n = arr.length;
+
+   for(let i = 0; i < n - 1; i++) {
+      for(let j = 0; j < n - 1 - i; j++) {
+         if(arr[j] > arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+
+         }
+      }
+   }
+   return arr;
+}
+
+console.log(bubbleSort([22, 10, 56, 43, 25, 120]));
+
